8bacf49afb25cd1acc182c89931580604a4a7e38
Luis Guilherme Redigolo Crosselli <103090304+luisguirc@users.noreply.github.com>
2024-04-18 20:58:44 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,23 +13,29 @@ const express = require('express');
 const { typeDefs } = require('./schema/type-defs.js');
 const { resolvers } = require('./schema/resolvers.js');
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const app = express();
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
 
-// Enable CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // You can set specific origins instead of '*'
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+  const app = express();
 
-server.applyMiddleware({ app });
+  // Enable CORS
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // You can set specific origins instead of '*'
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`API running at http://localhost:${PORT}${server.graphqlPath}`);
-});
+  server.applyMiddleware({ app });
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`API running at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
+
+startApolloServer();

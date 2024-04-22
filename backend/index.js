25281550432d1f36efe2e50389335b993870699f
Luis Guilherme Redigolo Crosselli <103090304+luisguirc@users.noreply.github.com>
2024-04-22 14:04:58 -0300

    Update index.js to use CORS

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,29 @@
+// const { ApolloServer } = require("apollo-server");
+// const { typeDefs } = require('./schema/type-defs.js');
+// const { resolvers } = require('./schema/resolvers.js');
+
+// const server = new ApolloServer({ typeDefs, resolvers });
+
+// server.listen().then(({ url }) => {
+//     console.log(`API running at ${url}`);
+// });
+
 const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require('./schema/type-defs.js');
 const { resolvers } = require('./schema/resolvers.js');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cors: {
+    origin: 'http://localhost:3000', // Allow requests from the frontend
+    credentials: true, // Allow sending cookies from frontend
+  },
+  // Disable HTTPS
+  https: false,
+});
+
+server.applyMiddleware({ app, cors: false });
 
 server.listen().then(({ url }) => {
     console.log(`API running at ${url}`);
